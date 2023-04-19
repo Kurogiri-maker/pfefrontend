f5fe0fd74aa1127ce4cf8a41d2132dc559df2412
ahmed tounsi <ahmed.tounsi178@gmail.com>
2023-04-19 09:38:24 +0100

    admin service modified

--- src/app/demo/service/admin.service.ts
+++ src/app/demo/service/admin.service.ts
@@ -17,14 +17,11 @@ export class AdminService {
     return this.http.get(`${this.apiUrl}/users?page=${currentPage}&size=${pageSize}`);
   }
 
-<<<<<<< HEAD
-  search(searchTerm: String) {
-=======
-  getHeader(){
+  getHeader() {
     return this.http.get<string[]>(`${this.apiUrl}/attributes`)
   }
 
-  
+
   saveUser(data: any): Observable<any> {
     return this.http.post(`${this.apiUrl}`, data);
   }
@@ -37,12 +34,7 @@ export class AdminService {
     return this.http.delete(`${this.apiUrl}/${IdDocument}`);
   }
 
-  search(searchTerm : String){
->>>>>>> 5fa47e1e9356786dcc9eeef613c886f0963406cf
+  search(searchTerm: String) {
     return this.http.get<string[]>(`${this.apiUrl}/users/find?searchTerm=%25${searchTerm}%25`);
-  }
-
-  getHeader() {
-    return this.http.get<string[]>(`${this.apiUrl}/attributes`)
   }
 }
