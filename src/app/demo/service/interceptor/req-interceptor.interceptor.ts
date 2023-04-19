65e8f6ec74e1bd102d4bf755957417a74a31b7fb
ahmed tounsi <ahmed.tounsi178@gmail.com>
2023-04-19 12:51:41 +0100

    logged in interceptor

--- src/app/demo/service/interceptor/req-interceptor.interceptor.ts
+++ src/app/demo/service/interceptor/req-interceptor.interceptor.ts
@@ -7,20 +7,27 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
+import { JwtClientService } from '../../components/auth/login/service/jwt-client.service';
 
 @Injectable()
 export class ReqInterceptor implements HttpInterceptor {
 
-  constructor(private cookieService: CookieService) { }
+  constructor(private cookieService: CookieService, private auth: JwtClientService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (request.url.match(/\/api\//)) {
+
+    if (request.url.match(/\/api\//) && this.auth.loggedIn()) {
+      // console.log("intercepted");
+
       const token = this.cookieService.get('token');
       console.log(token);
 
       return next.handle(request.clone({ setHeaders: { Authorization: `Bearer ${token}` } }));
     }
     else {
+      // if (!this.auth.loggedIn()) {
+      //   console.log("not logged in");
+      // }
       return next.handle(request);
     }
 
